7a715deac4b3ef735cb9671c8dcd3886f5c3833a
Nneji Obioma <nnejiobioma@gmail.com>
2024-02-09 13:31:44 +0100

    project completed for now

--- app/my-projects/page.tsx
+++ app/my-projects/page.tsx
@@ -1,23 +1,30 @@
-"use client"
-
 import ProjectCard from '@/components/ProjectCard'
 import { Projects } from '@/constants'
 import React from 'react'
 
 const Page = () => {
   return (
-    <div
-    style={{backgroundImage: "url(/mountains.jpg)"}}
-     className='w-screen h-screen flex items-center justify-center bg-center bg-cover'>
-      <div className='grid grid-cols-2 gap-5 max-w-[90%] max-h-[90%]'>
-        {Projects.map((project, index) => (
-          <ProjectCard
+    <div>
+      <video
+        autoPlay
+        loop
+        muted
+        style={{ width: "100%", height: "100%", objectFit: "cover", position: "fixed", zIndex: "-1" }}
+      >
+        <source src="/project2.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+      <div className='w-screen h-screen flex items-center justify-center bg-center bg-cover'>
+        <div className='flex flex-wrap justify-center gap-5 max-w-[70%] max-h-[70%] hover:overflow-y-scroll'>
+          {Projects.map((project, index) => (
+            <ProjectCard
               key={index}
               title={project.title}
               text={project.text}
               image={project.src}
-          />
-        ))}
+            />
+          ))}
+        </div>
       </div>
     </div>
   )
